d08d2e42ff69ddd31f4e32c8bd1d532b584c1077
Trisa Leung <trisaleung@Trisas-MacBook-Pro.local>
2020-06-12 20:03:57 -0700

    timer test

--- sprint_time.py
+++ sprint_time.py
@@ -1,89 +1,41 @@
 import discord
 from discord.ext import commands
+
 import time
 import datetime
-
 import schedule
-
 import config
+import threading
+
+async def timer(time_given):
+    global toc = time.time()
+
+    while True:
+        global tic = time.time()
+        if tic - toc >= (time_given * 60):
+            return
 
 @config.bot.event
-async def sprint_end(ctx, end_time):
-    print("DEBUGGING: SPRINT END")
-    # now = datetime.datetime.now()
-    
-    # end_minutes = now.minute + end_time
-    # end_seconds = now.second
+async def sprint_end(ctx, time_end):
+    timer(time_end)
 
-    # if end_seconds < 10:
-    #     await ctx.send("Sprint will end at **:" + str(end_minutes) + ":0" + str(end_seconds) + "**!")
-    # elif end_minutes < 10:
-    #     "Sprint will end at **:0" + str(end_minutes) + ":" + str(end_seconds) + "**!"
-    # else:
-    #     await ctx.send("Sprint will end at **:" + str(end_minutes) + ":" + str(end_seconds) + "**!")
-
-
-    # while end_time > 5:
-    #     time.sleep(1 * 60)
-    #     end_time -= 1
-    
-    # if end_time == 5:
-    #     await ctx.send("Sprint will begin in **" + str(end_time) + "** minute(s).")
-
-    # while end_time  > 3:
-    #     time.sleep(1 * 60)
-    #     end_time -= 1
-    
-    # if end_time == 3:
-    #     await ctx.send("Sprint will begin in **" + str(end_time) + "** minute(s).")
-
-    # while end_time > 1:
-    #     time.sleep(1 * 60)
-    #     end_time -= 1
-    
-    # if end_time == 1:
-    #     await ctx.send("Sprint will begin in **" + str(end_time) + "** minute.")
-
-    # time.sleep(end_time * 60)
-
-    # await ctx.send("Sprint has ended! Please enter your wcs!")
+    schedule.clear("timer")
+    await ctx.send("Sprint finished!")
 
 @config.bot.event
-async def start_sprint(ctx):
-    ctx.send("DEBUGGING: sprint starting!")
+async def sprint_start(ctx, time_start):
+    timer(time_start)
 
-    return schedule.CancelJob
-
+    await ctx.send("Sprint starting!")
+    schedule.clear("timer")
 
 @config.bot.event
-async def sprint_start(ctx, start_time):
-    print("DEBUGGING: SPRINT START")
+async def sprint_wait(ctx, start_time):
+    schedule.every(start_time).minutes.do(sprint_start).tag("timer")
 
-    print(schedule.every(start_time).minutes.do(start_sprint))
-
-    # while start_time > 5:
-    #     time.sleep(1 * 60)
-    #     start_time -= 1
-    
-    # if start_time == 5:
-    #     await ctx.send("Sprint will begin in **" + str(start_time) + "** minute(s).")
-
-    # while start_time > 3:
-    #     time.sleep(1 * 60)
-    #     start_time -= 1
-    
-    # if start_time == 3:
-    #     await ctx.send("Sprint will begin in **" + str(start_time) + "** minute(s).")
-
-    # while start_time > 1:
-    #     time.sleep(1 * 60)
-    #     start_time -= 1
-    
-    # if start_time == 1:
-    #     await ctx.send("Sprint will begin in **" + str(start_time) + "** minute.")
-
-    # time.sleep(start_time * 60)
-    # await ctx.send("Sprint will now begin! Start writing!")
+@config.bot.event
+async def sprint_run(ctx, end_time):
+    schedule.every(end_time).minutes.do(sprint_end).tag("timer")
 
 def only_one_sprint():
     #TODO: ensures there is only one sprint happening
--- writerbot.py
+++ writerbot.py
@@ -5,22 +5,11 @@ import config
 import sprint_time
 
 import schedule
-import asyncio
-
-import os
-from dotenv import load_dotenv
-project_folder = os.path.expanduser('~/Desktop/writerbot')  # adjust as appropriate
-load_dotenv(os.path.join(project_folder, '.env'))
-
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+import threading
 
 @config.bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(config.bot))
-
-@config.bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
 
 @config.bot.command()
 async def sprint(ctx, arg1, arg2, arg3):
@@ -29,12 +18,18 @@ async def sprint(ctx, arg1, arg2, arg3):
     elif int(arg1) < 0 and int(arg3) < 0:
         await ctx.send("Invalid command.")
     else:
-        await ctx.send("A sprint has been scheduled for {} minutes in {} minutes. Join sprint with ``-join <wc>``".format(arg1, arg3))
-        
+        await ctx.send(
+            "A sprint has been scheduled for {} minutes in {} minutes. Join sprint with ``-join <wc>``".format(
+                arg1, arg3))
+
+        await sprint_time.sprint_wait(ctx, int(arg3))
         await sprint_time.sprint_start(ctx, int(arg3))
-        #schedule.every(int(arg3)).minutes.do(sprint_time.sprint_start, ctx)
-        
+
+        await sprint_time.sprint_run(ctx, int(arg1))
         await sprint_time.sprint_end(ctx, int(arg1))
+
+        timer_thread = threading.Thread(target=sprint)
+        timer_thread.start()
 
 @config.bot.command()
 async def join(ctx, arg1):
@@ -48,4 +43,4 @@ async def time(ctx):
 async def quit(ctx):
     await ctx.send("Quitting sprint.")
 
-config.bot.run(DISCORD_TOKEN)
+config.bot.run("NzIwNzIzNDkzMzE0MTAxMjgw.XuPd2g.ISAyWLW_I3PAr51C8sXuWJcg6ZE")
