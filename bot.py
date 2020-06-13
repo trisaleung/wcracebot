7e08a144b55fdde39facc206b8cf69bea4e525ea
Trisa Leung <trisaleung@Trisas-MacBook-Pro.local>
2020-06-12 21:54:53 -0700

    fixed commits

--- bot.py
+++ bot.py
@@ -5,15 +5,13 @@ import config
 import sprint_time
 
 import schedule
-import asyncio
-bot = commands.Bot(command_prefix='-')
+import threading
 
-@bot.event
+@config.bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print('We have logged in as {0.user}'.format(config.bot))
 
-
-@bot.command()
+@config.bot.command()
 async def sprint(ctx, arg1, arg2, arg3):
     if arg2 != "in":
         await ctx.send("Invalid command.")
@@ -25,21 +23,13 @@ async def sprint(ctx, arg1, arg2, arg3):
                 arg1, arg3))
 
         await sprint_time.sprint_wait(ctx, int(arg3))
+        await sprint_time.sprint_start(ctx, int(arg3))
 
-        await sprint_time.sprint_start(ctx, int(arg1))
+        await sprint_time.sprint_run(ctx, int(arg1))
+        await sprint_time.sprint_end(ctx, int(arg1))
 
-        await sprint_time.sprint_end(ctx)
+@config.bot.command()
+async def prompt(ctx):
+    await ctx.send("prompt here.")
 
-@bot.command()
-async def join(ctx, arg1):
-    return
-
-@bot.command()
-async def time(ctx):
-    await ctx.send("Time remaining: ")
-
-@bot.command()
-async def quit(ctx):
-    await ctx.send("Quitting sprint.")
-
-bot.run("NzIwNzIzNDkzMzE0MTAxMjgw.XuPd2g.ISAyWLW_I3PAr51C8sXuWJcg6ZE")
+config.bot.run("XXXXXX")
